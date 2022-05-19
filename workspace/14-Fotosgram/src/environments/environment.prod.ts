a13db7a816ae72d52ceee5391519d1c0e0a6303a
Leandro C. Tomassini <leandroctomassini@gmail.com>
2022-05-19 17:27:52 -0300

    Chat

--- workspace/14-Fotosgram/src/environments/environment.prod.ts
+++ workspace/14-Fotosgram/src/environments/environment.prod.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: true,
-  url: 'http://192.168.32.1:4000'
+  url: 'https://fotosgrambackend.innovance.finance'
 };
--- workspace/14-Fotosgram/src/environments/environment.ts
+++ workspace/14-Fotosgram/src/environments/environment.ts
@@ -4,7 +4,7 @@
 
 export const environment = {
   production: false,
-  url: 'http://192.168.32.1:4000'
+  url: 'https://fotosgrambackend.innovance.finance'
 };
 
 /*
